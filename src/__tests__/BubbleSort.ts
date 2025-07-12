28bb785bf1d779e2c62a055588b6333d32e121b7
Theo Ribeiro <10909928+theorib@users.noreply.github.com>
2025-07-12 21:25:53 +0100

    ✅ test: add more test cases for bubble sort
    - add test case with unsorted array
    - add test case with empty array
    - add test case with single element array

--- src/__tests__/BubbleSort.ts
+++ src/__tests__/BubbleSort.ts
@@ -1,4 +1,4 @@
-import { it, expect } from "vitest"
+import { it, expect } from "vitest";
 import bubble_sort from "@code/BubbleSort";
 
 it("bubble-sort", function () {
@@ -9,3 +9,26 @@ it("bubble-sort", function () {
     expect(arr).toEqual([3, 4, 7, 9, 42, 69, 420]);
 });
 
+it("bubble-sort-2", function () {
+    const arr = [7, 6, 3, 4, 8, 2, 5, 9, 1, 0];
+
+    debugger;
+    bubble_sort(arr);
+    expect(arr).toEqual([0, 1, 2, 3, 4, 5, 6, 7, 8, 9]);
+});
+
+it("bubble-sort-3", function () {
+    const arr: number[] = [];
+
+    debugger;
+    bubble_sort(arr);
+    expect(arr).toEqual([]);
+});
+
+it("bubble-sort-4", function () {
+    const arr = [1];
+
+    debugger;
+    bubble_sort(arr);
+    expect(arr).toEqual([1]);
+});
