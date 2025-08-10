5442562b5996460daa57713a85a32dc8b6735695
Theo Ribeiro <10909928+theorib@users.noreply.github.com>
2025-08-10 20:35:57 +0100

    ✨ feat(tree): implement depth-first search on binary search tree
    - add `dfs` function to search for a target value in a binary search tree
    - solve challenge related to tree traversal

---
+++ src/day17/depthFirstSearchOnBinarySearchTree.ts
@@ -0,0 +1,17 @@
+/**
+ * Implement depth-first search on a binary search tree.
+ *
+ * Search for a target value in a binary search tree using depth-first search.
+ * @param head - Parameter of type BinaryNode<number>
+ * @param needle - Parameter of type number
+ * @returns Returns boolean
+ */
+export default function dfs(head: BinaryNode<number>, needle: number): boolean {
+    if (!head) return false;
+    if (head.value === needle) return true;
+
+    if (head.left && head.value > needle) return dfs(head.left, needle);
+    if (head.right && head.value < needle) return dfs(head.right, needle);
+
+    return false;
+}
